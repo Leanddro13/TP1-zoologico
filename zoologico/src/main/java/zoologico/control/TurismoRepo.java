19f84ec01011153793ae67a2e1b7880f1bddbe24
OutsiderRat <88382062+OutsiderRat@users.noreply.github.com>
2025-06-09 22:43:18 -0300

    Update TurismoRepo.java

--- zoologico/src/main/java/zoologico/control/TurismoRepo.java
+++ zoologico/src/main/java/zoologico/control/TurismoRepo.java
@@ -1,1 +1,58 @@
+package zoologico.control;
+
+import zoologico.model.Turismo;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class TurismoRepo {
+    private List<Turismo> turismos;
+
+    // Construtor
+    public TurismoRepo() {
+        this.turismos = new ArrayList<>();
+    }
+
+    // CREATE - Adicionar um novo Turismo
+    public void adicionarTurismo(Turismo turismo) {
+        turismos.add(turismo);
+        System.out.println("Turismo adicionado com sucesso!");
+    }
+
+    // READ - Obter todos os turismos
+
+    public List<Turismo> listarTurismos() {
+        return turismos;
+    }
+
+    // READ - Obter um turismo por íd
+
+    //public Turismo obterTurismoId(int id) {
+    //    if (indice >= 0 && indice < turismos.size()) {
+    //      return turismos.get(indice);
+    //  } else {
+    //      System.out.println("Índice inválido!");
+    //      return null;
+    //  }
+    //}
+
+    public void editarTurismo(int idTurismo, int novaDuracaoEstimada){
+        for(Turismo t: turismo){
+            if(t.getIdTurismo() == idTurismo){
+                t.setDuracaoEstimada(novaDuracaoEstimada);
+                System.out.println("Tour editada com sucesso.");
+                                
+            }
+        }
+}
+
+    // DELETE - Remover um turismo
+    public void removerTurismo(int idTurismo) {
+        turismos.removeIf(t -> t.getIdTurismo() == idTurismo);
+            System.out.println("Turismo removido com sucesso!");
+        } else {
+            System.out.println("Id inválido para remoção!");
+        }
+    }
+
 
